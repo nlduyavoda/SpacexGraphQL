529d9de9363cf94bab9bb32a86d9fe181db33114
leduy <leduy@VN-HCM-D-M-0022.local>
2022-02-16 18:06:24 +0700

    styling carts

--- src/component/Share/ButtonShared.tsx
+++ src/component/Share/ButtonShared.tsx
@@ -3,14 +3,14 @@ import styled from "styled-components";
 export const ButtonCart = styled.button`
   cursor: pointer;
   display: flex;
-  color: #fdeff4;
+  color: #92a9bd;
   border: none;
   width: 5rem;
   height: 5rem;
   border-radius: 50%;
   align-items: center;
   justify-content: center;
-  background-color: #92a9bd;
+  background-color: #eee;
   top: 2vh;
   right: 2vh;
   z-index: 2;
@@ -22,13 +22,4 @@ export const ButtonCart = styled.button`
     transition: ease-in 0.1s;
   }
 `;
-export const Carts = styled.div`
-  height: 0;
-  width: 0;
-  position: absolute;
-  background-color: white;
-  z-index: 3;
-  top: 110%;
-  right: 0;
-  transition: ease 0.25s;
-`;
+export const Carts = styled.div``;
--- src/component/Share/StyledComponent/index.tsx
+++ src/component/Share/StyledComponent/index.tsx
@@ -8,7 +8,26 @@ export default function ButtonStyled(props: any) {
   return (
     <ButtonCart className={className}>
       <Icon />
-      <Carts className={ClassName} />
+      <Carts className={ClassName}>
+        <div className="cart-item">item1</div>
+        <div className="cart-item">item2</div>
+        <div className="cart-item">item3</div>
+        <div className="cart-item">item1</div>
+        <div className="cart-item">item2</div>
+        <div className="cart-item">item3</div>
+        <div className="cart-item">item1</div>
+        <div className="cart-item">item2</div>
+        <div className="cart-item">item3</div>
+        <div className="cart-item">item1</div>
+        <div className="cart-item">item2</div>
+        <div className="cart-item">item3</div>
+        <div className="cart-item">item1</div>
+        <div className="cart-item">item2</div>
+        <div className="cart-item">item3</div>
+        <div className="cart-item">item1</div>
+        <div className="cart-item">item2</div>
+        <div className="cart-item">item3</div>
+      </Carts>
     </ButtonCart>
   );
 }
